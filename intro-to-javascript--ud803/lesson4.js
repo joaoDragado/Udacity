df03e95b1a319b9ed1501543325827a52dedb666
joaoDragado <dubmecoco@yahoo.com>
2018-03-03 19:43:32 +0200

    Lesson 4 stared.

---
+++ intro-to-javascript--ud803/lesson4.js
@@ -0,0 +1,66 @@
+// Loops
+/*
+var start = 0; // when to start
+while (start < 10) { // when to stop
+  console.log(start);
+  start = start + 2; // how to get to the next item
+}*/
+
+ /*var x = 1;
+
+while (x <= 20) {
+    if (((x % 3) === 0) && ((x % 5) === 0)) {
+        console.log('JuliaJames');
+    } 
+    else if ((x % 5) === 0) {
+        console.log('James');
+    }
+    else if ((x % 3) === 0) {
+        console.log('Julia');
+     }
+     else {
+        console.log(x);
+    }
+    x = x + 1
+ }   
+*/
+
+
+ /*var num = 99;
+
+while (num > 1) {
+     console.log(num + ' bottles of juice on the wall! ' + num + '  bottles of juice! Take one down, pass it around... ' + (num - 1) + ((num -1 !== 1) ? ' bottles' : ' bottle') + ' of juice on the wall!');
+     num -= 1;
+}
+console.log('1 bottle of juice on the wall! 1  bottle of juice! Take one down, pass it around... 0 bottles of juice on the wall!');
+*/
+
+
+/*var count = 60;
+while (count > -1) {
+    if (count === 50) {
+        console.log('Orbiter transfers from ground to internal power');
+    }
+    else if (count === 31) {
+        console.log('Ground launch sequencer is go for auto sequence start');
+    }
+    else if (count === 16) {
+        console.log('Activate launch pad sound suppression system');
+    }
+    else if (count === 10) {
+        console.log('Activate main engine hydrogen burnoff system');
+    }
+    else if (count === 6) {
+        console.log('Main engine start');
+    }
+    else if (count === 0) {
+        console.log('Solid rocket booster ignition and liftoff!');
+    } 
+    else  {
+        console.log('T-minus '+ count + ' seconds');
+    }
+    count -= 1;
+}
+*/
+
+
