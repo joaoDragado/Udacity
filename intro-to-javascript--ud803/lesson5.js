4e24f899e2df382b59bb7023213625e8b3b800b3
joaoDragado <dubmecoco@yahoo.com>
2018-03-04 18:38:01 +0200

    Lesson 5 started.

---
+++ intro-to-javascript--ud803/lesson5.js
@@ -0,0 +1,55 @@
+/*// declares the sayHello function
+function sayHello() {
+  var message = "Hello!"
+  return message; // returns value instead of printing it
+}
+
+// function returns "Hello!" and console.log prints the return value
+console.log(sayHello());
+*/
+
+
+// Quiz: Laugh it Off 1 (5-1)
+
+/*function laugh() {
+    var message = 'hahahahahahahahahaha!';
+    return message ;
+}
+console.log(laugh());
+*/
+
+
+//Quiz: Laugh it Off 2 (5-2)
+
+/*
+function laugh(num) {
+    var message = 'ha!';
+    for (var i = 1; i < num; i++) {
+        message = 'ha' + message ;
+    }
+    return message ;
+}
+console.log(laugh(2));
+*/
+
+
+// 
+
+/*function isThisWorking(input) {
+  console.log("Printing: isThisWorking was called and " + input + " was passed in as an argument.");
+  return "Returning: I am returning this string!";
+}
+
+isThisWorking(3);
+*/
+
+// Hoisting
+
+/*sayHi("Julia");
+
+function sayHi(name) {
+  console.log(greeting + " " + name);
+  var greeting;
+}
+*/
+
